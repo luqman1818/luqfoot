066eab827e636e4230532a1a5e0ec10450053459
luq18 <luqman.hrs@eduge.ch>
2025-04-03 19:52:06 +0200

    migration + model

---
+++ database/migrations/2025_04_03_174650_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+        $table->id('id_ord');
+        $table->date('date_ord');
+        $table->unsignedBigInteger('users_id_use');
+        $table->timestamps();
+
+        $table->foreign('users_id_use')->references('id_use')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2025_04_03_174906_create_order_shirts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_shirts', function (Blueprint $table) {
+            $table->unsignedBigInteger('shirts_id_shi');
+            $table->unsignedBigInteger('orders_id_ord');
+            $table->timestamps();
+    
+            $table->primary(['shirts_id_shi', 'orders_id_ord']);
+    
+            $table->foreign('shirts_id_shi')->references('id_shi')->on('shirts')->onDelete('cascade');
+            $table->foreign('orders_id_ord')->references('id_ord')->on('orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_shirts');
+    }
+};
